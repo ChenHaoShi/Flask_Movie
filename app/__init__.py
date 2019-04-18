02d506d6f66d058d5c67b45ff42e0e971b6cc71b
CHENHAOSHI_PC\Administrator <chenhaoshi@thewesthill.net>
2019-04-18 22:09:45 +0800

    更新后台前端布局及管理员登录逻辑代码

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
+import pymysql
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@127.0.0.1:3306/movie'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = '41fdcc3fe2b44557af31a6ed5cfff630'
+db = SQLAlchemy(app)
 
 from .home import home as home_blueprint
 from .admin import admin as admin_blueprint
--- app/models.py
+++ app/models.py
@@ -1,13 +1,5 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import pymysql
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@127.0.0.1:3306/movie'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-db = SQLAlchemy(app)
+from app import db
 
 # 会员信息
 class User(db.Model):
@@ -143,6 +135,10 @@ class Admin(db.Model):
 
     def __repr__(self):
         return '<Admin %r>' % self.name
+
+    def check_pwd(self, pwd):
+        from werkzeug.security import check_password_hash
+        return check_password_hash(self.pwd, pwd)
 
 # 管理员登录日志
 class Adminlog(db.Model):
